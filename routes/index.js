ffa7318b2924d1481abcd126cd7ad6981a163be3
Santiago Pavon <santiago@dit.upm.es>
2017-12-11 00:00:36 +0100

    Step 26 - Favourites.

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,7 @@ const quizController = require('../controllers/quiz');
 const tipController = require('../controllers/tip');
 const userController = require('../controllers/user');
 const sessionController = require('../controllers/session');
+const favouriteController = require('../controllers/favourite');
 
 //-----------------------------------------------------------
 
@@ -35,11 +36,11 @@ function saveBack(req, res, next) {
 
 // Restoration routes are GET routes that do not end in:
 //   /new, /edit, /play, /check, /session, or /:id.
-router.get('/',                        saveBack);
-router.get('/author',                  saveBack);
-router.get('/users',                   saveBack);
-router.get('/users/:id(\\d+)/quizzes', saveBack);
-router.get('/quizzes',                 saveBack);
+router.get('/',                           saveBack);
+router.get('/author',                     saveBack);
+router.get('/users',                      saveBack);
+router.get('/users/:id(\\d+)/quizzes',    saveBack);
+router.get('/quizzes',                    saveBack);
 
 //-----------------------------------------------------------
 
@@ -90,6 +91,7 @@ router.delete('/users/:userId(\\d+)',
     sessionController.adminOrMyselfRequired,
 	userController.destroy);
 
+
 router.get('/users/:userId(\\d+)/quizzes',
     sessionController.loginRequired,
     quizController.index);
@@ -121,13 +123,16 @@ router.delete('/quizzes/:quizId(\\d+)',
     quizController.adminOrAuthorRequired,
 	quizController.destroy);
 
-router.get('/quizzes/:quizId(\\d+)/play',  
+
+
+router.get('/quizzes/:quizId(\\d+)/play',
 	quizController.play);
 router.get('/quizzes/:quizId(\\d+)/check', 
 	quizController.check);
 
 
-router.get('/quizzes/:quizId(\\d+)/tips/new',  
+
+router.get('/quizzes/:quizId(\\d+)/tips/new',
     sessionController.loginRequired,
 	tipController.new);
 router.post('/quizzes/:quizId(\\d+)/tips',     
@@ -141,6 +146,17 @@ router.delete('/quizzes/:quizId(\\d+)/tips/:tipId(\\d+)',
     sessionController.loginRequired,
     quizController.adminOrAuthorRequired,
     tipController.destroy);
+
+
+// Routes for the resource favourites of a user
+router.put('/users/:userId(\\d+)/favourites/:quizId(\\d+)',
+    sessionController.loginRequired,
+    sessionController.adminOrMyselfRequired,
+    favouriteController.add);
+router.delete('/users/:userId(\\d+)/favourites/:quizId(\\d+)',
+    sessionController.loginRequired,
+    sessionController.adminOrMyselfRequired,
+    favouriteController.del);
 
 
 module.exports = router;
